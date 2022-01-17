2b71314a8e8603fdcb4f3a150fde48f49d9fc214
stnava <stnava@gmail.com>
2022-01-17 13:20:41 -0500

    ENH: preliminary analysis

--- src/analyze.R
+++ src/analyze.R
@@ -11,13 +11,23 @@ mydf$numericGrade[mydf$grade =='b']=4
 mydf$numericGrade[mydf$grade =='c']=2
 mydf$numericGrade[mydf$grade =='f']=0
 nms=names(mydf)
+# mydf=mydf[ mydf$grade %in% c("a","b","c"),]
 istrain = sample( 1:nrow(mydf), round(0.9*nrow(mydf)) )
+mydf$grade=factor(mydf$grade)
 mydftr=mydf[istrain,]
 mydfte=mydf[-istrain,]
+classer=FALSE
 myform = as.formula( paste( "numericGrade ~ ", paste0( nms[c(12:15, 22:24)], collapse='+') ) )
-myform = as.formula( paste( "numericGrade ~ ", paste0( nms[c( 22:24)], collapse='+') ) )
+if ( classer )
+  myform = as.formula( paste( "grade ~ ", paste0( nms[c(12:15, 22:24)], collapse='+') ) )
 mdl = lm(  myform , data=mydftr )
 pred = predict( mdl, newdata=mydfte )
-rtemis::mplot3.xy( mydfte$numericGrade, pred, fit=T, se.fit=T )
-print(summary(mdl))
-print( cor.test( mydfte$numericGrade, pred, type='k' ))
+if ( classer ) {
+  mdl = randomForest(  myform , data=mydftr )
+  pred = predict( mdl, newdata=mydfte )
+  print( caret::confusionMatrix( pred, mydfte$grade ) )
+} else {
+  rtemis::mplot3.xy( mydfte$numericGrade, pred, fit=T, se.fit=T )
+  print(summary(mdl))
+  print( cor.test( mydfte$numericGrade, pred, type='k' ))
+}
